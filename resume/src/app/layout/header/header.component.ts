91a6e2f4c87eefd323042bac330ebdf00f1d7df6
vladik <vladislav4042003@mail.ru>
2024-05-08 19:37:49 +0300

    add header

--- resume/src/app/layout/header/header.component.ts
+++ resume/src/app/layout/header/header.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import {NgForOf} from "@angular/common";
+import {RouterLink, RouterLinkActive} from "@angular/router";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf,
+    RouterLinkActive,
+    RouterLink
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
 
+  navigateItems: Array<any> = [
+    { name: 'Home', customUrl: '/'},
+    { name: 'About', customUrl: '/'},
+    { name: 'Portfolio', customUrl: '/'},
+    { name: 'Contact', customUrl: '/'}
+  ]
 }
